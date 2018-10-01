f9d449abf77256f03e7a1690e510fbf51f788aa1
Kris E. Lentz <kris.e.lentz@gmail.com>
2018-10-01 18:51:36 -0500

    replace react-bootstrap with reactstrap

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,38 +1,43 @@
 import React from 'react'; 
-import { Collapse } from 'react-bootstrap';
+import { Collapse } from 'reactstrap';
 import '../css/Title.css'
 
 class Title extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {open: false};
+    this.state = { collapse: false };
+    this.toggle = this.toggle.bind(this)
   }
 
-render() {
-	const link = this.state.open ? "less info" : "more info"
-
-    return (
-      <div className='title-div'>
-      	<h1 className='title'>Microwave Converter</h1>
-      	<h2>Find correct cooking times for your own microwave</h2>
-        <a className='toggle' onClick={ ()=> this.setState({ open: !this.state.open })}>
-  			{link}
-        </a>
-        <Collapse in={this.state.open}>
-          <div>
-            <p>
-              The cooking times for microwavable foods are for specific microwave wattages. Use this application to find out the correct cooking time for your own microwave.
-            </p>  
-            <p>
-              The power of most microwaves is between 600 and 1200 watts.
-            </p>
-          </div>
-        </Collapse>
-      </div>
-
-    );
+  toggle() {
+    this.setState({collapse: !this.state.collapse })
   }
+ 
+  render() {
+  	  const link = this.state.collapse ? "less info" : "more info"
 
-};
+      return (
+        <div className='title-div'>
+        	<h1 className='title'>Microwave Converter</h1>
+        	<h2>Find correct cooking times for your own microwave</h2>
+          <a onClick={this.toggle}>
+    			{link}
+          </a>
+          <Collapse isOpen={this.state.collapse}>
+            <div>
+              <p>
+                The cooking times for microwavable foods are for specific microwave wattages. Use this application to find out the correct cooking time for your own microwave.
+              </p>  
+              <p>
+                The power of most microwaves is between 600 and 1200 watts.
+              </p>
+            </div>
+          </Collapse>
+        </div>
+
+      );
+    }
+
+  };
 
 export default Title;
